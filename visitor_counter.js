e5de5aee810cdbdf40e9368235ab5ca660b2c12b
Sasha Lapommeray <sashalapommeray@proton.me>
2025-08-13 22:19:45 -0400

    fix please

--- visitor_counter.js
+++ visitor_counter.js
@@ -1,6 +1,18 @@
 function checkVisitor() {
   const visited = localStorage.getItem("visited") === "true";
   const counterEl = document.getElementById("counter");
+  const messageEl = document.getElementById("visitor-message");
+
+  console.log("Visited?", visited);
+  console.log("Message element found?", !!messageEl);
+
+
+  if (messageEl) {
+    messageEl.innerText = visited
+      ? "Total visitors:"
+      : "You are visitor number";
+    console.log("Message set to:", messageEl.innerText);
+  }
 
   if (!visited) {
     fetch("https://44gkxl6irc.execute-api.us-east-1.amazonaws.com/Prod/counter", {
@@ -8,28 +20,21 @@ function checkVisitor() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ visited: false })
     })
-    .then(res => res.json())
-    .then(data => {
-      localStorage.setItem("visited", "true");
-      
-      if (counterEl) {
-        counterEl.innerText = data;
-      }
-    })
-    .catch(err => {
-      console.error("Failed to fetch counter:", err);
-    });
-  } 
-
-  else {
-    if (counterEl) {
-      fetch("https://44gkxl6irc.execute-api.us-east-1.amazonaws.com/Prod/counter", {
-        method: "GET"
-      })
       .then(res => res.json())
-      .then(data => counterEl.innerText = data)
+      .then(data => {
+        localStorage.setItem("visited", "true");
+        if (counterEl) counterEl.innerText = data;
+      })
       .catch(err => console.error("Failed to fetch counter:", err));
-    }
+  } else {
+    fetch("https://44gkxl6irc.execute-api.us-east-1.amazonaws.com/Prod/counter", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (counterEl) counterEl.innerText = data;
+      })
+      .catch(err => console.error("Failed to fetch counter:", err));
   }
 }
 
